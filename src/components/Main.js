e876bcbc22fa404bbbea8c03727ba7fa20bd59ef
reneetanoco <renee.tanoco@gmail.com>
2023-08-26 17:29:53 +0800

    data fetch for all timeframe

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,75 +3,69 @@ import React, { useState, useEffect } from 'react';
 
 export default function Main() {
     const [data, setData] = useState([]);
+    const [selectBtn, setselectBtn] = useState('daily');
 
-    useEffect(() =>{
+    useEffect(() => {
         const fetchData = async () => {
-            try{
+            try {
                 const res = await fetch('/data.json');
-                if(!res.ok){
+                if (!res.ok) {
                     console.log('error');
                 }
-    
+
                 const jsonData = await res.json();
                 setData(jsonData)
             }
-            catch(error){
+            catch (error) {
                 console.error('Error fetching data:', error);
             }
         }
         fetchData();
-    },[]);
+    }, []);
+    const handleBtn = (select) => {
+        setselectBtn(select);
+    }
+    const selectTime = (title) => {
+        
+        if (selectBtn === 'daily') {
+            const matchingItem = data.find(item => item.title === title);
+            if (matchingItem) {
+                return matchingItem.timeframes.daily.current;
+            }
+        }
+        else if (selectBtn === 'weekly') {
+            const matchingItem = data.find(item => item.title === title);
+            if (matchingItem) {
+                return matchingItem.timeframes.weekly.current;
+            }
+        }
+        else if (selectBtn === 'monthly') {
+            const matchingItem = data.find(item => item.title === title);
+            if (matchingItem) {
+                return matchingItem.timeframes.monthly.current;
+            }
+        }
+        return 'Title not found';
+    }
+
     return (
         <>
             <main>
-                <div class="card">
+                <div className="card">
                     <p>Report for<span> Jeremy Robson</span></p>
-                    <button>Daily</button>
-                    <button>Weekly</button>
-                    <button>Monthly</button>
+                    <button onClick={() => handleBtn('daily')}>Daily</button>
+                    <button onClick={() => handleBtn('weekly')}>Weekly</button>
+                    <button onClick={() => handleBtn('monthly')}>Monthly</button>
                 </div>
-                <div class="card">
-                    <div class="wrap">
-                        <h4>Work</h4>
-                        <div class="data-cur"></div>
-                        <p>Last week - <span class="data-prev">{data[0].timeframes.weekly.current} hours</span></p>
+                {data.map((item, index) => (
+                    <div className="card" key={index}>
+                        <div className="wrap">
+                            <h4>{item.title}</h4>
+                            <div className="data-cur"></div>
+                            <p>Last week - <span className="data-prev">{selectTime(item.title)}</span></p>
+                        </div>
                     </div>
-                </div>
-                <div class="card">
-                    <div class="wrap">
-                        <h4>Play</h4>
-                        <div class="data-cur"></div>
-                        <p>Last week - <span class="data-prev">{data[1].timeframes.weekly.current} hours</span></p>
-                    </div>
-                </div>
-                <div class="card">
-                    <div class="wrap">
-                        <h4>Study</h4>
-                        <div class="data-cur"></div>
-                        <p>Last week - <span class="data-prev">{data[2].timeframes.weekly.current} hours</span></p>
-                    </div>
-                </div>
-                <div class="card">
-                    <div class="wrap">
-                        <h4>Exercise</h4>
-                        <div class="data-cur"></div>
-                        <p>Last week - <span class="data-prev">{data[3].timeframes.weekly.current} hours</span></p>
-                    </div>
-                </div>
-                <div class="card">
-                    <div class="wrap">
-                        <h4>Social</h4>
-                        <div class="data-cur"></div>
-                        <p>Last week - <span class="data-prev">{data[4].timeframes.weekly.current} hours</span></p>
-                    </div>
-                </div>
-                <div class="card">
-                    <div class="wrap">
-                        <h4>Self Care</h4>
-                        <div class="data-cur"></div>
-                        <p>Last week - <span class="data-prev">{data[5].timeframes.weekly.current} hours</span></p>
-                    </div>
-                </div>
+                ))}
             </main>
         </>
     )
